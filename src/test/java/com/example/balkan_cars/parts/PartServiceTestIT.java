7e4c0761a86e60db1da633bd0c163c92b4c28213
georgi-g-likov <georgilikov2@gmail.com>
2026-01-14 23:35:14 +0200

    Removing decorators
    Removing dumb decorators from columns

--- src/test/java/com/example/balkan_cars/parts/PartServiceTestIT.java
+++ src/test/java/com/example/balkan_cars/parts/PartServiceTestIT.java
@@ -3,7 +3,6 @@ package com.example.balkan_cars.parts;
 import com.example.balkan_cars.defined.cars.DefinedCarsDto;
 import com.example.balkan_cars.enums.State;
 import com.example.balkan_cars.enums.SubCategory;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
@@ -21,9 +20,9 @@ class PartServiceTestIT {
     private PartService serviceToTest;
     @Autowired
     private PartRepository partRepository;
-    
+
     private final UUID businessId = UUID.randomUUID();
-    
+
     @Test
     void createPart_shouldSaveTheListOfImagesAsSingleString() {
         PartDto result = serviceToTest.create(createPartDto());
@@ -31,10 +30,10 @@ class PartServiceTestIT {
         Part part = partRepository.getByBusinessId(businessId);
         assertEquals("ph.jpg,ph2.jpg,ph3.jpg", part.getImageUri());
     }
-    
+
     private PartDto createPartDto() {
         return new PartDto(businessId, "brake pads", new DefinedCarsDto(UUID.randomUUID(),
-                "BMW" , "M3", (short)2020), SubCategory.BRAKES, State.NEW, 450.20,
-                "bmw","selling brake pads",(byte)2 , List.of("ph.jpg", "ph2.jpg", "ph3.jpg"));
+                "BMW", "M3", (short) 2020), SubCategory.BRAKES, State.NEW, 450.20,
+                "bmw", "selling brake pads", (byte) 2, List.of("ph.jpg", "ph2.jpg", "ph3.jpg"));
     }
 }
