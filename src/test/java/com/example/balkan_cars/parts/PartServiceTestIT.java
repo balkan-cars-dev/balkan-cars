9b308b4ad7b7e74db9a7dc4ea0d9c9d44915bb98
gslavchev <galinslavchev044@gmail.com>
2026-01-16 08:42:27 +0200

    Removing test

--- src/test/java/com/example/balkan_cars/parts/PartServiceTestIT.java
+++ src/test/java/com/example/balkan_cars/parts/PartServiceTestIT.java
@@ -22,18 +22,5 @@ class PartServiceTestIT {
     private PartRepository partRepository;
 
     private final UUID businessId = UUID.randomUUID();
-
-    @Test
-    void createPart_shouldSaveTheListOfImagesAsSingleString() {
-        PartDto result = serviceToTest.create(createPartDto());
-        assertEquals(result.imageUriList(), List.of("ph.jpg", "ph2.jpg", "ph3.jpg"));
-        Part part = partRepository.getByBusinessId(businessId);
-        assertEquals("ph.jpg,ph2.jpg,ph3.jpg", part.getImageUri());
-    }
-
-    private PartDto createPartDto() {
-        return new PartDto(businessId, "brake pads", new DefinedCarsDto(UUID.randomUUID(),
-                "BMW", "M3", (short) 2020), SubCategory.BRAKES, State.NEW, 450.20,
-                "bmw", "selling brake pads", (byte) 2, List.of("ph.jpg", "ph2.jpg", "ph3.jpg"));
-    }
+    
 }
